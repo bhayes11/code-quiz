fdb7b4757930278e7d7bb5c0be0904435bf144e3
Hayes11 <bhayes81@gmail.com>
2020-09-28 16:33:30 -0500

    added btn to start page with diplay none event

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hello");
+//console.log("hello");
+var startRow = document.getElementById("start-page");
+var startButton = document.getElementById("start-btn");
+
+startButton.addEventListener("click", function () {
+  startRow.style.display = "none";
+});

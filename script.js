d37cbd8d82dd073c5d2e0161dbf60f5f53efd64c
Hayes11 <bhayes81@gmail.com>
2020-09-29 10:22:38 -0500

    added first question buttons

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 //console.log("hello");
+
 var startRow = document.getElementById("start-page");
 var startButton = document.getElementById("start-btn");
+var quesOptions = document.getElementById("ques-selections");
+var quesOptions = document.getElementById("ques-options");
 
 startButton.addEventListener("click", function () {
   startRow.style.display = "none";
+  renderQuesOptions();
 });
+
+function renderQuesOptions() {
+  var availableChoices = ["strings", "booleans", "alerts", "numbers"];
+
+  for (var i = 0; i < availableChoices.length; i++) {
+    var button = document.createElement("button");
+
+    button.setAttribute("class", "btn btn-info");
+    button.textContent = availableChoices[i];
+    button.setAttribute("data-value", availableChoices[i]);
+
+    quesOptions.append(button);
+  }
+}

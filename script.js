14d3cdb9d912059984086d49dce3133202e46a1d
Hayes11 <bhayes81@gmail.com>
2020-09-30 17:16:37 -0500

    added numbers to choices

--- script.js
+++ script.js
@@ -9,31 +9,41 @@ var currentStage = 0;
 var stages = [
   {
     question: "Commonly used data types DO NOT include:",
-    options: ["strings", "booleans", "alerts", "numbers"],
+    options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
   },
   {
     question:
       "The condition in an if / else statement is enclosed within ____.",
-    options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    options: [
+      "1. quotes",
+      "2. curly brackets",
+      "3. parentheses",
+      "4. square brackets",
+    ],
   },
   {
     question: "Arrays in JavaScript can be used to store ____.",
     options: [
-      "numbers and strings",
-      "other arrays",
-      "booleans",
-      "all of the above",
+      "1. numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      "4. all of the above",
     ],
   },
   {
     question:
       "String values must be enclosed within ____ when being assigned to variables.",
-    options: ["commas", "curly brackets", "quotes", "parentheses"],
+    options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
   },
   {
     question:
       "A very useful tool for used during development and debugging printing content to the debugger is:",
-    options: ["JavaScript", "terminal / bash", "for loops", "console log"],
+    options: [
+      "1. JavaScript",
+      "2. terminal / bash",
+      "3. for loops",
+      "4. console log",
+    ],
   },
 ];
 //Timer function
